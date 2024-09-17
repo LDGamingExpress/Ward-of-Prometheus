2dd0ba8e44a165f85634a81c3936adbf6164aa87
LDGamingExpress <110698883+LDGamingExpress@users.noreply.github.com>
2024-09-16 23:11:18 -0400

    Start
    Very basic start: main menu, start of base game with a starter spritesheet, camera movement for the player

---
+++ ward-of-prometheus/PlayerScript.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_pressed("Left"):
+		velocity.x -= SPEED
+	if Input.is_action_pressed("Right"):
+		velocity.x += SPEED
+	if Input.is_action_pressed("Up"):
+		velocity.y -= SPEED
+	if Input.is_action_pressed("Down"):
+		velocity.y += SPEED
+
+	move_and_slide()
+	
+	velocity.x = velocity.x / 100
+	velocity.y = velocity.y / 100
+	if velocity.y < 10:
+		velocity.y = 0
+	if velocity.x < 10:
+		velocity.x = 0
