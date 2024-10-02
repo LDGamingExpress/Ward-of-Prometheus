52527cc5e993c99f20a7ad8afba1e79f114e3113
LDGamingExpress <110698883+LDGamingExpress@users.noreply.github.com>
2024-10-02 00:39:18 -0400

    Small Fix

--- ward-of-prometheus/start_buttonScript.gd
+++ ward-of-prometheus/start_buttonScript.gd
@@ -3,7 +3,8 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Globals.FoolsTrapped = 0
+	Globals.FoolTotal = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
